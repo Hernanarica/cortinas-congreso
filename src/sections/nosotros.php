15e3cee323d1729bbaca5b1f65734e7abf5cf71f
Hernán <hernanaricammm@gmail.com>
2021-05-03 04:38:12 +0100

    Sección Nosotros terminado

--- src/sections/nosotros.php
+++ src/sections/nosotros.php
@@ -1,3 +1,72 @@
-<main>
-	<h1>Nosotros</h1>
-</main>
+<section class="nosotros">
+	<div class="nosotros__banner"></div>
+	<p class="nosotros__text">
+		Somos una empresa familiar con más de 20 años de experiencia, llevamos garantía de confianza a los hogares.
+		<br>
+		Trabajamos con las marcas líderes de la industria, asegurando la mejor calidad de nuestros productos y servicios.
+	</p>
+	<p class="nosotros__title">Tenemos 2 sucursales en CABA:</p>
+	<div class="info">
+		<div class="info__content">
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__locate-dark.svg" alt="icon">
+					Sucursal Montserrat:
+				</div>
+				<div class="info__text">
+					Combate de los pozos 618.
+				</div>
+			</div>
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__time.svg" alt="icon">
+					Horario de atención
+				</div>
+				<div class="info__text">
+					Lunes a Viernes de 10 a 18hs
+					<br>
+					Sábados de 10 a 13hs.
+				</div>
+			</div>
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__whatsapp.svg" alt="icon">
+					Whatsapp
+				</div>
+				<div class="info__text">
+					1153198818
+				</div>
+			</div>
+		</div>
+		<div class="info__content">
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__locate-dark.svg" alt="icon">
+					Sucursal Belgráno:
+				</div>
+				<div class="info__text">
+					Ibera 2509.
+				</div>
+			</div>
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__time.svg" alt="icon">
+					Horario de atención
+				</div>
+				<div class="info__text">
+					Lunes a Viernes de 10 a 18hs
+					<br>
+					Sábados de 10 a 13hs.
+				</div>
+			</div>
+			<div class="info__item">
+				<div class="info__title">
+					<img src="src/assets/icons/icon__whatsapp.svg" alt="icon">
+					Whatsapp
+				</div>
+				<div class="info__text">
+					1153198818
+				</div>
+			</div>
+		</div>
+</section>
