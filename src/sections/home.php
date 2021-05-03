f81907eb3f0abdcbd40b2a042fbc8ceccf1a6241
Hernán <hernanaricammm@gmail.com>
2021-05-03 03:29:32 +0100

    Sección Home terminada

--- src/sections/home.php
+++ src/sections/home.php
@@ -39,12 +39,46 @@
 		<div class="description__content">
 			<div class="description">
 				<h3 class="description__title">Cortinas de PVC</h3>
-				<p class="banner__text">Son una alternativa a tener en cuenta por su bajo costo y mantenimiento</p>
+				<p class="description__text">Son una alternativa a tener en cuenta por su bajo costo y mantenimiento</p>
 				<a href="#" class="description__btn">
 					<img src="src/assets/icons/icon__eye.svg" alt="icon eye" class="description__icon">
 					Ver más
 				</a>
 			</div>
 		</div>
+	</div>
+	<div class="banner2">
+		<div class="description__content">
+			<div class="description">
+				<h3 class="description__title">Cortinas Madera</h3>
+				<p class="description__text">La belleza de la madera proporciona una agradable sensación ambiental gracias a sus vetas y tonalidades a la par de ser un resistente
+					cerramiento para sus ventanas.
+				</p>
+				<a href="#" class="description__btn">
+					<img src="src/assets/icons/icon__eye.svg" alt="icon eye" class="description__icon">
+					Ver más
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="banner3">
+		<div class="description__content">
+			<div class="description">
+				<h3 class="description__title">Cortinas Barriot</h3>
+				<p class="description__text">Permiten regular el paso de la luz manteniendo la misma privacidad a través de la posición de las varillas.</p>
+				<a href="#" class="description__btn">
+					<img src="src/assets/icons/icon__eye.svg" alt="icon eye" class="description__icon">
+					Ver más
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="clients">
+		<p class="clients__title">Clientes que confían en nosotros:</p>
+		<picture>
+			<source media="(min-width: 1280px)" srcset="src/assets/imgs/clientes-pc.png"/>
+			<source media="(min-width: 640px)" srcset="src/assets/imgs/clientes-tablet.png"/>
+			<img src="src/assets/imgs/clientes-mob.png" alt="clientes logos" class="clients__image">
+		</picture>
 	</div>
 </section>
